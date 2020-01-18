fd4e16a1367cea0bcc2f2a0473a98efba2f95196
Philip Shen <philipshen13@gmail.com>
2020-01-18 14:24:41 -0800

    Set up project w/ babel and webpack

---
+++ webpack.common.js
@@ -0,0 +1,64 @@
+const CopyPlugin = require('copy-webpack-plugin')
+const DoteEnvPlugin = require('dotenv-webpack')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const PostCSSFlexbugsFixes = require('postcss-flexbugs-fixes')
+const PostCSSNormalize = require('postcss-normalize')
+const PostCSSPresetEnv = require('postcss-preset-env')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+/**
+ * Config options that apply to both prod & dev
+ */
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: require.resolve('css-loader'),
+                        options: {
+                            importLoaders: 1,
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: require.resolve('postcss-loader'),
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                PostCSSFlexbugsFixes,
+                                PostCSSPresetEnv({
+                                    stage: 3,
+                                }),
+                                PostCSSNormalize,
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin([
+            {
+                from: 'public',
+                to: '',
+            },
+        ]),
+        new DoteEnvPlugin(),
+        new ManifestPlugin(),
+    ],
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+}
