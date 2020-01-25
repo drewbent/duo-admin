7970405a0a17893713b3c259639de4b249d0c154
Philip Shen <philipshen13@gmail.com>
2020-01-25 09:50:50 -0800

    Remove es6 code from server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /**
  * Script that will serve our file on heroku
  */
-import express from 'express'
-import path from 'path'
+const express = require('express')
+const path = require('path')
 
 const port = process.env.PORT || 8080
 const app = express()
