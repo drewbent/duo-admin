b879095d9411b1f667352a317b45fff05b74759a
Philip Shen <philipshen13@gmail.com>
2020-02-18 21:26:52 -0800

    checkpoint; going to push to heroku

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -6,7 +6,7 @@ export const formatDateBackend = date => dateformat(date, 'yyyy-mm-dd')
 
 export const formatTime = date => dateformat(date, 'HH:MM')
 
-export const formatDateTime = date => dateformat(date, 'dd/mm/yyyy HH:MM')
+export const formatDateTime = date => dateformat(date, 'mm/dd/yyyy HH:MM')
 
 export const areSameDate = (date1, date2) => {
   return formatDate(date1) === formatDate(date2)
